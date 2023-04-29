7e23e80edd3d098c344f973fc7dedd2f4c6fa670
sajad <sajadkamali017@gmail.com>
2023-04-29 22:55:29 +0330

    feat: find ball by yolo method.

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
 # Football Goal Detection
-<br>
 
 ## About
 ### Hough circle method
 This Python project uses OpenCV to detect whether a goal is present in a series of images. It does this by identifying circles in the images that match the size and shape of a football goal, and then checking if they are located above or below a line that is drawn based on the position of the goalposts.
 
+### YOLO method
+This project contains a Python script that implements an algorithm to detect a football goal in a video sequence. The algorithm uses computer vision techniques and object detection models to identify the goalposts and crossbar of a football goal.
+
 ## Installation
 
+The following libraries are required to run the script:
+
+- opencv-python
+- numpy
+- ultralytics
+
+You can install them using pip:
 ```
-pip install opencv-python
-pip install numpy
+pip install opencv-python numpy ultralytics
 ```
 
 ## Usage
@@ -21,6 +29,30 @@ The FootballGoalDetection class has three methods:
 - get_image_and_processed_edges(name): Returns the resized image and the edges processed from it.
 - is_goal_detected(x_cent, y_cent, radius): Returns True if the circle detected inside the goalpost region is determined to be a goal, False otherwise.
 - goal_detector(): Loops through a series of images, detects circles using the HoughCircles function in OpenCV, and checks if the detected circles are goals.
+
+### YOLO method
+The detection algorithm works as follows:
+
+1- Load an image of a football goal.
+
+2- Convert the image to grayscale.
+
+3- Apply edge detection to the grayscale image.
+
+4- Crop a sensitive area around the goal from the edge-detected image.
+
+5- Apply Hough line detection to the cropped image to find the line that represents the top of the goal.
+
+6- Calculate the slope and y-intercept of the line.
+
+7- For each frame in the video sequence:
+
+- Apply object detection to the frame using the YOLOv5 model.
+For each detected object:
+
+- If the object is a goalpost or crossbar, calculate its center and radius.
+- If the calculated center and radius indicate that a goal has been scored, print a message indicating which frame contains the goal.
+- The algorithm assumes that the camera is stationary and that the goal does not move during the video sequence. It also assumes that the goal is fully visible in each frame of the video sequence.
 
 By default, this will loop through six images (res/1.jpg through res/6.jpg) and print a message to the console when a goal is detected.
 Sample:
--- find_ball_by_yolo.py
+++ find_ball_by_yolo.py
@@ -1,6 +1,67 @@
 import cv2
 from ultralytics import YOLO
+import numpy as np
 
-model = YOLO("models/yolov8n")
-results = model.predict(source=0, show=True)
+class FootballGoalDetection():
 
+    def __init__(self):
+        self.sensitive_area_x1 = 190
+        self.sensitive_area_y1 = 140
+        self.sensitive_area_x2 = 520
+        self.sensitive_area_y2 = 355
+        self.m, self.b = self.get_gate_sens_line_m_b()
+        self.model = YOLO("models/yolov8x")
+     
+    def get_gate_sens_line_m_b(self):
+        img = cv2.imread('res/2.jpg')
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+        ltl_edg = edges[self.sensitive_area_y1:self.sensitive_area_y2, self.sensitive_area_x1:self.sensitive_area_x2]
+        lines = cv2.HoughLinesP(ltl_edg, 1, np.pi/180, 100, minLineLength=100, maxLineGap=10)
+        min_x = 0
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            m = abs((y2 - y1) / (x2 - x1))
+            if 0.5 < m < 1 and min_x < x1:
+                min_x = x1
+                b = -m * (self.sensitive_area_x1 + x1) + (self.sensitive_area_y1 + y1)
+                return m, b
+            
+    def is_goal_detected(self, x_cent, y_cent, radius):
+        m, b = self.m, self.b
+        yhat = x_cent * m + b
+        if yhat > y_cent:
+            return False
+        else:
+            distance = abs(m*x_cent-y_cent+b)/(m**2+1)**0.5
+            return distance > radius
+
+    def goal_detector(self):
+        is_goal_detected = False
+        for i in range(1, 7):
+            img = cv2.imread(f"res/{i}.jpg")
+            results = self.model.predict(source=img)
+            for result in results:
+                for (obj_xyxy, obj_cls) in zip(result.boxes.xyxy, result.boxes.cls):
+                    obj_cls = int(obj_cls)
+                    x1 = obj_xyxy[0].item()
+                    y1 = obj_xyxy[1].item()
+                    x2 = obj_xyxy[2].item()
+                    y2 = obj_xyxy[3].item()
+                    # cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 1)
+                    if obj_cls == 32:
+                        x_cent = (x2+x1)/2
+                        y_cent = (y2+y1)/2
+                        radius = ((x2-x1)/2+(y2-y1)/2)/2
+                        is_goal_detected = self.is_goal_detected(x_cent, y_cent, radius)
+                        if is_goal_detected:
+                            print(f"Goal detected in frame name: {i}")
+
+            # cv2.imshow("img", img)
+            # cv2.waitKey(0)
+
+        if not is_goal_detected:
+            print(f"No Goal detected.")
+
+football_goal_detection = FootballGoalDetection()
+football_goal_detection.goal_detector()
